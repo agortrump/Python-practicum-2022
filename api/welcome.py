9f9abbc4c7aad27253969875b0f017d0b9304294
agortrump <agortrump@gmail.com>
2022-12-23 15:10:27 +0200

    Second try

--- api/welcome.py
+++ api/welcome.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
 
 @app.route('/')
@@ -24,4 +24,4 @@ def welcome_back():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
