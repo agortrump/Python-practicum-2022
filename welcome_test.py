a5f0776e4a4ceba79795f2b0eff30294c23a5f34
agortrump <agortrump@gmail.com>
2023-01-01 20:33:43 +0200

    New Approach working
    Python script rewritten
    Html rewritten, included bootstraps and css, also base.html page

--- welcome_test.py
+++ welcome_test.py
@@ -45,18 +45,20 @@ def get_weather(city):
 
 
 @app.route('/weather', methods=['POST', 'GET'])
-def get_city(city=city):
+def get_city(city='Tallinn'):
     # Get input from HTML form
     if request.method == 'POST':
         city = request.form.get('city_name').capitalize()
         # If nothing is typed in form return weather page
         if len(city) == 0:
-            return render_template('weather.html', no_city="You should write the name of a city in the box")
+            return render_template('weather.html', no_input="You should write the name of a city in the box!")
+        elif city not in get_weather(city).values():
+            return render_template('weather.html', no_city='Could not find such city')
     return (render_template('weather.html', temp=get_weather(city)['main']['temp'],
                             feels_like=get_weather(city)['main']['feels_like'],
                             wind=get_weather(city)['wind']['speed'],
                             city=city,
-                            country=get_weather(city)['sys']['country'],
+                            country=', ' + get_weather(city)['sys']['country'],
                             icon='https://openweathermap.org/img/wn/' + get_weather(city)['weather'][0]['icon'] + '@2x.png'),
             city)
 
